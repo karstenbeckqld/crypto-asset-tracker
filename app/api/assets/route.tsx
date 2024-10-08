fed8ebdee0190be5de9f5f1eb11d5e6fe492b4a5
Karsten Beck <s3912792@student.rmit.edu.au>
2024-10-08 13:01:53 +1000

    .env usage

--- app/api/assets/route.tsx
+++ app/api/assets/route.tsx
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     // Make a get request to the coin market API using the API key as custom header.
     const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest ', {
         headers: {
-            'X-CMC_PRO_API_KEY': '1281325f-c9b5-4f1f-99bb-a8d9a3105ad6', // Put in.env file later.
+            'X-CMC_PRO_API_KEY': process.env.COIN_MARKET_API_KEY, // Put in.env file later.
         },
     });
 
