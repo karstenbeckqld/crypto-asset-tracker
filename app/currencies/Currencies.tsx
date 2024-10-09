281fd071309fc039aa1547144dc4e7660c815a77
Karsten Beck <s3912792@student.rmit.edu.au>
2024-10-09 20:00:27 +1000

    Deployment Errors Stage 2

--- app/currencies/Currencies.tsx
+++ app/currencies/Currencies.tsx
@@ -1,26 +1,21 @@
-'use client';
-// Because of the user interaction for sorting and searching, this component will be rendered on the client side.
+'use client'; // Ensure this is at the top to force client-side rendering
 
-import { useRouter, useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation'; // Client-side hooks
+import { Input, Spinner, Search2Icon, TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
 import CoinToken from '@/app/components/token';
 import useTokens from '@/app/react-query/hooks/useTokens';
 import { TokenType } from "@/app/types/CryptoTypes";
-import { Input, Search2Icon, Spinner, TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
 
-// A Function to display the arrow according to the sort order for the active sorting column.
+// Function to display sorting arrow
 function displayArrow(sortKey: string, activeSort: string, order: string) {
     if (sortKey !== activeSort) {
         return <TriangleUpIcon />;
     }
-
-    return order === 'asc' ? (
-        <TriangleUpIcon color='purple.500' />
-    ) : (
-        <TriangleDownIcon color='purple.500' />
-    );
+    return order === 'asc' ? <TriangleUpIcon color='purple.500' /> : <TriangleDownIcon color='purple.500' />;
 }
 
+// Header display function
 function displayHeader() {
     return (
         <div id='text' className='flex flex-row items-center w-full p-2.5'>
@@ -32,6 +27,7 @@ function displayHeader() {
     );
 }
 
+// Search field display function
 function displaySearchField(searchQuery: string, setSearchQuery: (value: string) => void) {
     return (
         <div id='search' className='flex flex-row items-center w-full p-2.5 mt-2.5'>
@@ -40,7 +36,7 @@ function displaySearchField(searchQuery: string, setSearchQuery: (value: string)
                 color='white'
                 placeholder='Search by name or symbol...'
                 value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)} // Update search query on input change
+                onChange={(e) => setSearchQuery(e.target.value)}
             />
         </div>
     );
@@ -49,64 +45,33 @@ function displaySearchField(searchQuery: string, setSearchQuery: (value: string)
 const Currencies = () => {
     const [showSearch, setShowSearch] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
-    const [isClient, setIsClient] = useState(false); // Track if we are on the client
-    const { data, error } = useTokens(); // Fetching token data
-    // const router = useRouter(); // useRouter from next/navigation
+    const [isClient, setIsClient] = useState(false); // Track if we're on the client side
+    const { data, error } = useTokens(); // Fetch tokens using a custom hook
+    const router = useRouter();
 
+    const searchParams = useSearchParams(); // Search params hook
+
+    // Toggle search display
     const toggleDisplay = () => {
         setShowSearch(!showSearch);
         setSearchQuery("");
-    }
+    };
 
     // Ensure this hook runs only on the client side
     useEffect(() => {
         setIsClient(true);
     }, []);
 
-    // If we are on the server side, render nothing
+    // If we're not on the client, render a spinner (or nothing)
     if (!isClient) {
-        return <Spinner />; // Or return a loading spinner if you want
+        return <Spinner />;
     }
 
-    return (
-        <div>
-            {/* Error handling */}
-            {error && <div>Error: {error.message}</div>}
-
-            <div className='flex flex-row items-center w-full pl-6 h-10 pt-4 mb-3'>
-                {showSearch ? displaySearchField(searchQuery, setSearchQuery) : displayHeader()}
-                <button
-                    onClick={toggleDisplay}
-                    className={showSearch
-                        ? `order-first bg-[#1E1E1E] p-2.5 text-[#ffffff] mt-4`
-                        : `order-last bg-[#1E1E1E] p-2.5 text-[#ffffff] mt-4`}
-                >
-                    <Search2Icon />
-                </button>
-            </div>
-
-            {/* Sorting buttons */}
-            <div className='flex px-8 py-1 bg-[#1E1E1E] mb-2.5 text-[#ffffff]'>
-                <Suspense fallback={<Spinner />}>
-                    <SortButtons />
-                </Suspense>
-            </div>
-
-            {/* Token list */}
-            <Suspense fallback={<Spinner />}>
-                <TokenList data={data || []} searchQuery={searchQuery} />
-            </Suspense>
-        </div>
-    );
-};
-
-const SortButtons = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-
+    // Get the sorting key and order from the URL search params
     const sort = searchParams.get('sort') || 'cmc_rank';
     const order = searchParams.get('order') || 'asc';
 
+    // Handle sorting logic
     const handleSort = (key: string) => {
         const newOrder = order === 'asc' ? 'desc' : 'asc';
         const params = new URLSearchParams(searchParams);
@@ -115,25 +80,7 @@ const SortButtons = () => {
         router.push(`?${params.toString()}`);
     };
 
-    return (
-        <>
-            <button className='flex-none w-1/5 ml-0' onClick={() => handleSort('cmc_rank')}>
-                # {displayArrow('cmc_rank', sort, order)}
-            </button>
-            <button className='w-1/4' onClick={() => handleSort('name')}>
-                Name {displayArrow('name', sort, order)}
-            </button>
-            <button className='w-1/5' onClick={() => handleSort('price')}>
-                Price {displayArrow('price', sort, order)}
-            </button>
-            <button className='w-1/4' onClick={() => handleSort('percent_change_24h')}>
-                24h % {displayArrow('percent_change_24h', sort, order)}
-            </button>
-        </>
-    );
-};
-
-const TokenList = ({ data, searchQuery }: { data: TokenType[], searchQuery: string }) => {
+    // Sorting function for tokens
     const sortData = (tokens: TokenType[], key: string, order: 'asc' | 'desc') => {
         return [...tokens].sort((a, b) => {
             let aValue, bValue;
@@ -158,34 +105,67 @@ const TokenList = ({ data, searchQuery }: { data: TokenType[], searchQuery: stri
         });
     };
 
+    // Filter and sort tokens based on search query and sorting params
     const filteredTokens = data ? data.filter((token) =>
         token.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
         token.symbol.toLowerCase().includes(searchQuery.toLowerCase())
     ) : [];
 
-    const searchParams = useSearchParams();
-    const sort = searchParams.get('sort') || 'cmc_rank';
-    const order = searchParams.get('order') || 'asc';
-
     const sortedTokens = sortData(filteredTokens, sort as string, order as 'asc' | 'desc');
 
     return (
-        <div className='token-list'>
-            {sortedTokens.length > 0 ? (
-                sortedTokens.map(token => (
-                    <CoinToken
-                        key={token.cmc_rank}
-                        cmc_rank={token.cmc_rank}
-                        name={token.name}
-                        price={token.quote.USD.price}
-                        percent_change_24h={token.quote.USD.percent_change_24h}
-                        symbol={token.symbol}
-                        market_cap_dominance={token.quote.USD.market_cap_dominance}
-                    />
-                ))
-            ) : (
-                <p>No data available</p>
-            )}
+        <div>
+            {error && <div>Error: {error.message}</div>}
+
+            {/* Header and search toggle */}
+            <div className='flex flex-row items-center w-full pl-6 h-10 pt-4 mb-3'>
+                {showSearch ? displaySearchField(searchQuery, setSearchQuery) : displayHeader()}
+                <button
+                    onClick={toggleDisplay}
+                    className={showSearch
+                        ? `order-first bg-[#1E1E1E] p-2.5 text-[#ffffff] mt-4`
+                        : `order-last bg-[#1E1E1E] p-2.5 text-[#ffffff] mt-4`}
+                >
+                    <Search2Icon />
+                </button>
+            </div>
+
+            {/* Sorting buttons */}
+            <div className='flex px-8 py-1 bg-[#1E1E1E] mb-2.5 text-[#ffffff]'>
+                <Suspense fallback={<Spinner />}>
+                    <button className='flex-none w-1/5 ml-0' onClick={() => handleSort('cmc_rank')}>
+                        # {displayArrow('cmc_rank', sort, order)}
+                    </button>
+                    <button className='w-1/4' onClick={() => handleSort('name')}>
+                        Name {displayArrow('name', sort, order)}
+                    </button>
+                    <button className='w-1/5' onClick={() => handleSort('price')}>
+                        Price {displayArrow('price', sort, order)}
+                    </button>
+                    <button className='w-1/4' onClick={() => handleSort('percent_change_24h')}>
+                        24h % {displayArrow('percent_change_24h', sort, order)}
+                    </button>
+                </Suspense>
+            </div>
+
+            {/* Token list */}
+            <div className='token-list'>
+                {sortedTokens.length > 0 ? (
+                    sortedTokens.map(token => (
+                        <CoinToken
+                            key={token.cmc_rank}
+                            cmc_rank={token.cmc_rank}
+                            name={token.name}
+                            price={token.quote.USD.price}
+                            percent_change_24h={token.quote.USD.percent_change_24h}
+                            symbol={token.symbol}
+                            market_cap_dominance={token.quote.USD.market_cap_dominance}
+                        />
+                    ))
+                ) : (
+                    <p>No data available</p>
+                )}
+            </div>
         </div>
     );
 };
